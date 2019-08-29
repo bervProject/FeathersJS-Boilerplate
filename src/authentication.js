6f957cfc377ab5296188048d6080dfed243b9b37
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2019-08-29 11:49:51 +0700

    Update Authenticate and user to use role

--- src/authentication.js
+++ src/authentication.js
@@ -1,9 +1,26 @@
-const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
+const {
+  AuthenticationService,
+  JWTStrategy
+} = require('@feathersjs/authentication');
 const { LocalStrategy } = require('@feathersjs/authentication-local');
 const { expressOauth } = require('@feathersjs/authentication-oauth');
 
+class MyAuthenticationService extends AuthenticationService {
+  async getPayload(authResult, params) {
+    // Call original `getPayload` first
+    const payload = await super.getPayload(authResult, params);
+    const { user } = authResult;
+
+    return Object.assign(payload, {
+      userId: user.id,
+      role: user.role,
+      roles: user.role
+    });
+  }
+}
+
 module.exports = app => {
-  const authentication = new AuthenticationService(app);
+  const authentication = new MyAuthenticationService(app);
 
   authentication.register('jwt', new JWTStrategy());
   authentication.register('local', new LocalStrategy());
--- src/models/users.model.js
+++ src/models/users.model.js
@@ -6,7 +6,6 @@ const DataTypes = Sequelize.DataTypes;
 module.exports = function (app) {
   const sequelizeClient = app.get('sequelizeClient');
   const users = sequelizeClient.define('users', {
-  
     email: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -16,8 +15,10 @@ module.exports = function (app) {
       type: DataTypes.STRING,
       allowNull: false
     },
-  
-  
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     hooks: {
       beforeCount(options) {
